977acc73862fd4b9d6a391b3e9d920154111e9a3
Adebowale <adebowaletemitayo40@gmail.com>
2023-09-21 20:54:38 +0100

    implementing the worldwiwse project

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import {
   MapContainer,
   TileLayer,
